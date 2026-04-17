2ef32896ce2e087549e851ea5f09f7778d012c73
BokChoy45 <anguyencoco2003@gmail.com>
2026-04-16 22:11:52 -0500

    change back

--- Hw3.js
+++ Hw3.js
@@ -351,8 +351,7 @@ function showAlert() {
 //Validate Info
 function validateInfo() {
     let valid = true;
-        document.getElementById("submit").disable = true
-
+    
     if (!validateFname()) {
         valid = false;
     }
